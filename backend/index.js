415430d6b1ef970da5db1864d3c27428aa32099d
kdotmanoj <hsuper681@gmail.com>
2025-08-06 03:35:47 +0530

    Add POST /jobs route to insert new job into database

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,47 @@
 const express = require('express');
+const pool = require('./database')
 const app = express();
 
-app.get('/', (request,response) => {
-    response.send('Hello World!');
-})
+app.use(express.json());
 
-app.listen(5000)
+app.post('/jobs', async (request,response) => {
+    const {
+        job_title,
+        company_name,
+        salary,
+        description,
+        application_status,
+        date_applied
+    } = request.body;
+
+    if(!job_title || !company_name){
+        return response.status(400).json({error: 'Title and company are required'});
+    }
+
+    try{
+        const [result] = await pool.query(
+            `INSERT INTO jobs (
+                job_title,
+                company_name,
+                salary,description,
+                application_status,
+                date_applied
+            ) VALUES (?,?,?,?,?,?)`,
+            [
+                job_title,
+                company_name,
+                salary || null,
+                description || null,
+                application_status || 'Applied',
+                date_applied || null
+            ]
+        );
+        response.status(201).json({message: 'Job added succesfully'});
+    }
+    catch(error){
+        console.error('Error inserting job:', error);
+        response.status(500).json({error: 'Internal server error'});
+    }
+});
+
+app.listen(5000);
